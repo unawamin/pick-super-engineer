fb24d27539f0635938457f01d4745c54b6294a08
saasbook <saasbook@saasbook.(none)>
2014-10-30 00:11:35 -0300

    add FeatureC

--- main.rb
+++ main.rb
@@ -7,6 +7,20 @@ def random_pick
   end
 
   puts eng_list.shuffle.sample #print one name of engineerlist by random
+  pick_fetureC(eng_list)
+end
+
+def pick_fetureC(list_name)
+  search = gets.chomp()
+  puts "Search name '#{search}'"
+  #i = 0
+  #while i < list_name.size
+  for i in 0...list_name.size
+    if list_name[i].include? "#{@search}"
+	puts list_name[i]
+    end
+    break
+  end
 end
 
 random_pick()
