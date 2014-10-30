572214afec0d41ad342fc793c27315f34f3fc4e9
saasbook <saasbook@saasbook.(none)>
2014-10-29 22:36:52 -0300

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,12 @@
+def random_pick 
+  inputs = File.open("engineerlist.txt") #open file input
+  eng_list = Array.new() #create new array name eng_list
+
+  for x in inputs 
+    eng_list.push(x) #set each  data of input to array name eng_list
+  end
+
+  puts eng_list.shuffle.sample #print one name of engineerlist by random
+end
+
+random_pick()
